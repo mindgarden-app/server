dbdc9d4986ccf28901d5b1fe9539b3a79813fa99
jiyoung lee <bestjy1202@nate.com>
2019-07-06 18:58:46 +0900

    DiaryList 수정

--- routes/diarylist/diarylist.js
+++ routes/diarylist/diarylist.js
@@ -1,4 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../../module/pool');
+const util = require('../../module/utils');
+const statusCode = require('../../module/statusCode');
+const resMessage = require('../../module/responseMessage');
+
+router.get('/', async(req, res) => {
+    try{
+        const userIdx = req.body.userIdx;
+        const date = req.body.date;
+        const selectEpisodeCmtQuery = 'SELECT * FROM diary WHERE wpIdx = ? ORDER BY writetime DESC';
+        const selectEpisodeCmtResult = await db.queryParam_Arr(selectEpisodeCmtQuery, [req.params.epIdx]);
+
+            if (!selectEpisodeCmtResult) {
+                res.status(200).send(utils.successFalse(statusCode.DB_ERROR, resMessage.CMT_DB_SELECT_ERROR));
+            } else {
+                res.status(200).send(utils.successTrue(statusCode.OK, resMessage.COMMENT_SELECTED, selectEpisodeCmtResult));
+            }
+        }catch(err){
+            console.log(err);
+        }
+});
 
 module.exports = router;
