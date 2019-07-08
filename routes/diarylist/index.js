caedb4247abac231b2bec73091fdda2f94e8a759
jungahshin <shinasong@naver.com>
2019-07-08 18:46:30 +0900

    diarylist/index 수정

--- routes/diarylist/index.js
+++ routes/diarylist/index.js
@@ -4,6 +4,5 @@ var router = express.Router();
 router.use('/', require('./diarylist'));
 router.use('/click', require('./click'));
 router.use('/delete', require('./delete'));
-router.use('/sort', require('./sort'));
 
 module.exports = router;
