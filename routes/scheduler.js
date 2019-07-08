4c628f9c685d21143bf7b2eeade945a72f2e86a9
jungahshin <shinasong@naver.com>
2019-07-09 01:16:45 +0900

    scheduler 수정

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -14,4 +14,55 @@ cron.schedule('0 0 0 * * *', async() => {//자정에
     console.log(updateBalloonResult);
 })
 
+cron.schedule('0 0 0 1 2 *', async() => {//매달 1일 00시에
+    //29일(2), 30일(4, 6, 9, 11), 31일(1, 3, 5, 7, 8, 10, 12)
+    //3개 insert(5,21,30), treeIdx-16, all user
+    const insertGardenQuery_1 = 'INSERT INTO garden (date, location, treeIdx, userIdx) VALUES (?, ?, ?, ?)';
+    const insertGardenQuery_2 = 'INSERT INTO garden (date, location, treeIdx, userIdx) VALUES (?, ?, ?, ?)';
+    const insertGardenQuery_3 = 'INSERT INTO garden (date, location, treeIdx, userIdx) VALUES (?, ?, ?, ?)';
+    const insertTransaction = await db.Transaction(async(connection) => {
+        const garden_date = moment().format("YYYY-MM-DD ddd");
+        const selectUserIdxQuery = 'SELECT userIdx FROM user';
+        const selectUserIdxResult = await db.queryParam_None(selectUserIdxQuery);
+        for(i=0;i<selectUserIdxResult.length;i++){
+            const insertGardenResult_1 = await db.queryParam_Parse(insertGardenQuery_1, [garden_date, 5, 16, selectUserIdxResult[i]['userIdx']]);
+            const insertGardenResult_2 = await db.queryParam_Parse(insertGardenQuery_2, [garden_date, 21, 16, selectUserIdxResult[i]['userIdx']]);
+            const insertGardenResult_3 = await db.queryParam_Parse(insertGardenQuery_3, [garden_date, 30, 16, selectUserIdxResult[i]['userIdx']]);
+        }
+    });
+    console.log(insertTransaction);
+})
+
+cron.schedule('0 0 0 1 4,6,9,11 *', async() => {//매달 1일 00시에
+    //29일(2), 30일(4, 6, 9, 11), 31일(1, 3, 5, 7, 8, 10, 12)
+    //2개 insert(21,30), treeIdx-16, all user
+    const insertGardenQuery_1 = 'INSERT INTO garden (date, location, treeIdx, userIdx) VALUES (?, ?, ?, ?)';
+    const insertGardenQuery_2 = 'INSERT INTO garden (date, location, treeIdx, userIdx) VALUES (?, ?, ?, ?)';
+    const insertTransaction = await db.Transaction(async(connection) => {
+        const garden_date = moment().format("YYYY-MM-DD ddd");
+        const selectUserIdxQuery = 'SELECT userIdx FROM user';
+        const selectUserIdxResult = await db.queryParam_None(selectUserIdxQuery);
+        for(i=0;i<selectUserIdxResult.length;i++){
+            const insertGardenResult_1 = await db.queryParam_Parse(insertGardenQuery_1, [garden_date, 21, 16, selectUserIdxResult[i]['userIdx']]);
+            const insertGardenResult_2 = await db.queryParam_Parse(insertGardenQuery_2, [garden_date, 30, 16, selectUserIdxResult[i]['userIdx']]);
+        }
+    });
+    console.log(insertTransaction);
+})
+
+cron.schedule('0 0 0 1 1,3,5,7,8,10,12 *', async() => {//매달 1일 00시에
+    //29일(2), 30일(4, 6, 9, 11), 31일(1, 3, 5, 7, 8, 10, 12)
+    //1개 insert(30), treeIdx-16, all user
+    const insertGardenQuery_1 = 'INSERT INTO garden (date, location, treeIdx, userIdx) VALUES (?, ?, ?, ?)';
+    const insertTransaction = await db.Transaction(async(connection) => {
+        const garden_date = moment().format("YYYY-MM-DD ddd");
+        const selectUserIdxQuery = 'SELECT userIdx FROM user';
+        const selectUserIdxResult = await db.queryParam_None(selectUserIdxQuery);
+        for(i=0;i<selectUserIdxResult.length;i++){
+            const insertGardenResult_1 = await db.queryParam_Parse(insertGardenQuery_1, [garden_date, 30, 16, selectUserIdxResult[i]['userIdx']]);
+        }
+    });
+    console.log(insertTransaction);
+})
+
 module.exports = router;
